8338c3132c46428cf5fe0bd136cf80f8e33cafcf
momosenda <momo.senda@rakus.co.jp>
2018-11-19 10:43:47 +0900

    carクラスの機能追加

--- src/main/java/jp/co/sample/Car.java
+++ src/main/java/jp/co/sample/Car.java
@@ -1,5 +1,18 @@
 package jp.co.sample;
 
 public class Car {
+	private int speed =0;
+	
+	public void hello() {
+		System.out.println("うえむーこんにちは");
+	}
+	
+	public int getSpeed() {
+		return speed;
+	}
 
+	public void setSpeed(int speed) {
+		this.speed = speed;
+	}
+	
 }
