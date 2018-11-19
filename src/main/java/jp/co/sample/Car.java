ca0f463428ac0841d3481f5fbde17c9c1fdc87ec
uemurakento <kento.uemura@rakus.co.jp>
2018-11-19 10:42:56 +0900

    Carクラス修正（スピードフィールド追加、runメソッド追加）

--- src/main/java/jp/co/sample/Car.java
+++ src/main/java/jp/co/sample/Car.java
@@ -1,5 +1,18 @@
 package jp.co.sample;
 
 public class Car {
+	private int speed = 2;
+
+	public void run() {
+		speed += 10;
+	}
+
+	public int getSpeed() {
+		return speed;
+	}
+
+	public void setSpeed(int speed) {
+		this.speed = speed;
+	}
 
 }
